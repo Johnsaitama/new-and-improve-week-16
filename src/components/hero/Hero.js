33d72703e5e55feb051d9d87e24d6656d849e32f
john <jonathanluffy675@gmail.com>
2023-11-02 18:45:50 -0700

    update

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './HeroStyles.css'
+
+const Hero =() => {
+    return(
+        <div className= 'hero'>
+            <div className="container">
+                
+                <div className="content">
+                <h2><span>Welcone</span> feel free to scroll down </h2>
+                <h2>Data</h2>
+                <h2 className='blue'>Secured</h2>
+                <h2>Protection</h2>
+                <div><button>Learn More</button></div>
+            </div>
+           </div>
+            </div>
+            
+    )
+}
+export default Hero
